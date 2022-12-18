ba2f21c89b4c485527f386cc981b4ea1acc67996
Diogo Pereira <josediogogp@gmail.com>
2022-12-18 02:21:27 +0000

    feature_delete
    Addead feature to delete

--- app.js
+++ app.js
@@ -54,8 +54,10 @@ class Bd {
 			if(despesa === null) {
 				continue
 			}
+			despesa.id = i
 			despesas.push(despesa)
 		}
+		
 		return despesas
 	}
 
@@ -87,6 +89,10 @@ class Bd {
 		console.log(despesasFiltradas)
 
 		return despesasFiltradas
+	}
+
+	remover(id) {
+		localStorage.removeItem(id)
 	}
 }
 
@@ -165,6 +171,19 @@ function carregaListaDespesas(despesas = Array(), filtro = false) {
 
 		linha.insertCell(2).innerHTML = d.descricao
 		linha.insertCell(3).innerHTML = `${d.valor}€`
+
+		let btn = document.createElement("button")
+		btn.className = 'btn btn-danger'
+		btn.innerHTML = '<i class="fas fa-times"></i>'
+		btn.id = 'id_despesa_' + d.id
+		btn.onclick = function() {
+			
+			let id = this.id.replace('id_despesa_', '')
+			
+			bd.remover(id)
+			window.location.reload()
+		}
+		linha.insertCell(4).append(btn)
 
 	})
 
